b8d08f9455ade2718aee08e9ba6c5fb5d94d4707
Desktop\Chander <nikhilchander96@gmail.com>
2021-02-23 15:30:40 +0100

    Allineamento: Privacy aggiunto all git

---
+++ DRT_Traduzioni_ExcelToJson/grunt/excel_to_json.js
@@ -0,0 +1,227 @@
+
+module.exports = {
+    icb: {
+        options: {
+
+        },
+        files: [{
+            expand: true,
+            cwd: './src/app/main/icb/i18n',
+            dest: './src/app/main/icb/i18n',
+            ext: '.json',
+            src: [
+                '**/Translations.xlsx'
+            ]
+        }],
+
+    },
+
+    dcb: {
+        options: {
+
+        },
+        files: [{
+            expand: true,
+            cwd: './src/app/main/dcb/i18n',
+            dest: './src/app/main/dcb/i18n',
+            ext: '.json',
+            src: [
+                '**/Translations.xlsx'
+            ]
+        }],
+
+    },
+
+    privacy: {
+        options: {
+
+        },
+        files: [{
+            expand: true,
+            cwd: './src/app/main/icb/privacy/i18n',
+            dest: './src/app/main/icb/privacy/i18n',
+            ext: '.json',
+            src: [
+                '**/Translations.xlsx'
+            ]
+        }],
+
+    },
+
+    profile: {
+        options: {
+
+        },
+        files: [{
+            expand: true,
+            cwd: './src/app/main/profile/i18n',
+            dest: './src/app/main/profile/i18n',
+            ext: '.json',
+            src: [
+                '**/Translations.xlsx'
+            ]
+        }],
+
+    },
+
+    content: {
+        options: {
+
+        },
+        files: [{
+            expand: true,
+            cwd: './src/app/main/content/i18n',
+            dest: './src/app/main/content/i18n',
+            ext: '.json',
+            src: [
+                '**/Translations.xlsx'
+            ]
+        }],
+
+    },
+
+    navigation: {
+        options: {
+
+        },
+        files: [{
+            expand: true,
+            cwd: './src/app/navigation/i18n',
+            dest: './src/app/navigation/i18n',
+            ext: '.json',
+            src: [
+                '**/Translations.xlsx'
+            ]
+        }],
+
+    },
+
+    toolbar: {
+        options: {
+
+        },
+        files: [{
+            expand: true,
+            cwd: './src/app/toolbar/i18n',
+            dest: './src/app/toolbar/i18n',
+            ext: '.json',
+            src: [
+                '**/Translations.xlsx'
+            ]
+        }],
+
+    },
+
+    bottombar: {
+        options: {
+
+        },
+        files: [{
+            expand: true,
+            cwd: './src/app/bottombar/i18n',
+            dest: './src/app/bottombar/i18n',
+            ext: '.json',
+            src: [
+                '**/Translations.xlsx'
+            ]
+        }],
+
+    },
+
+    quickpanel: {
+        options: {
+
+        },
+        files: [{
+            expand: true,
+            cwd: './src/app/quick-panel/i18n',
+            dest: './src/app/quick-panel/i18n',
+            ext: '.json',
+            src: [
+                '**/Translations.xlsx'
+            ]
+        }],
+
+    },
+
+    navbar: {
+        options: {
+
+        },
+        files: [{
+            expand: true,
+            cwd: './src/app/navbar/i18n',
+            dest: './src/app/navbar/i18n',
+            ext: '.json',
+            src: [
+                '**/Translations.xlsx'
+            ]
+        }],
+
+    },
+
+    pos: {
+        options: {
+
+        },
+        files: [{
+            expand: true,
+            cwd: './src/app/main/pos/i18n',
+            dest: './src/app/main/pos/i18n',
+            ext: '.json',
+            src: [
+                '**/Translations.xlsx'
+            ]
+        }],
+
+    },
+
+    messenger: {
+        options: {
+
+        },
+        files: [{
+            expand: true,
+            cwd: './src/app/main/messenger/i18n',
+            dest: './src/app/main/messenger/i18n',
+            ext: '.json',
+            src: [
+                '**/Translations.xlsx'
+            ]
+        }],
+
+    },
+
+    chat: {
+        options: {
+
+        },
+        files: [{
+            expand: true,
+            cwd: './src/app/main/messenger/chat/i18n',
+            dest: './src/app/main/messenger/chat/i18n',
+            ext: '.json',
+            src: [
+                '**/Translations.xlsx'
+            ]
+        }],
+
+    },
+
+    store: {
+        options: {
+
+        },
+        files: [{
+            expand: true,
+            cwd: './src/app/main/store/i18n',
+            dest: './src/app/main/store/i18n',
+            ext: '.json',
+            src: [
+                '**/Translations.xlsx'
+            ]
+        }],
+
+    }
+}
+
---
+++ DRT_Traduzioni_ExcelToJson/grunt/json_to_excel.js
@@ -0,0 +1,211 @@
+module.exports = {
+    icb: {
+
+        options: {
+
+        },
+        files: [{
+            expand: true,
+            cwd: './src/app/main/icb/i18n',
+            dest: './src/app/main/icb/i18n',
+            src: [
+                '**/*.json'
+            ]
+        }],
+    },
+
+    dcb: {
+        
+        options: {
+
+        },
+        files: [{
+            expand: true,
+            cwd: './src/app/main/dcb/i18n',
+            dest: './src/app/main/dcb/i18n',
+            src: [
+                '**/*.json'
+            ]
+        }],
+    },
+
+    privacy: {
+
+        options: {
+
+        },
+        files: [{
+            expand: true,
+            cwd: './src/app/main/icb/privacy/i18n',
+            dest: './src/app/main/icb/privacy/i18n',
+            src: [
+                '**/*.json'
+            ]
+        }],
+    },
+
+    content: {
+
+        options: {
+
+        },
+        files: [{
+            expand: true,
+            cwd: './src/app/main/content/i18n',
+            dest: './src/app/main/content/i18n',
+            src: [
+                '**/*.json'
+            ]
+        }],
+    },
+
+    pos: {
+
+        options: {
+
+        },
+        files: [{
+            expand: true,
+            cwd: './src/app/main/pos/i18n',
+            dest: './src/app/main/pos/i18n',
+            src: [
+                '**/*.json'
+            ]
+        }],
+    },
+
+    profile: {
+
+        options: {
+
+        },
+        files: [{
+            expand: true,
+            cwd: './src/app/main/profile/i18n',
+            dest: './src/app/main/profile/i18n',
+            src: [
+                '**/*.json'
+            ]
+        }],
+    },
+
+    navigation: {
+
+        options: {
+
+        },
+        files: [{
+            expand: true,
+            cwd: './src/app/navigation/i18n',
+            dest: './src/app/navigation/i18n',
+            src: [
+                '**/*.json'
+            ]
+        }],
+    },
+
+    toolbar: {
+
+        options: {
+
+        },
+        files: [{
+            expand: true,
+            cwd: './src/app/toolbar/i18n',
+            dest: './src/app/toolbar/i18n',
+            src: [
+                '**/*.json'
+            ]
+        }],
+    },
+
+    bottombar: {
+
+        options: {
+
+        },
+        files: [{
+            expand: true,
+            cwd: './src/app/bottombar/i18n',
+            dest: './src/app/bottombar/i18n',
+            src: [
+                '**/*.json'
+            ]
+        }],
+    },
+
+    quickpanel: {
+
+        options: {
+
+        },
+        files: [{
+            expand: true,
+            cwd: './src/app/quick-panel/i18n',
+            dest: './src/app/quick-panel/i18n',
+            src: [
+                '**/*.json'
+            ]
+        }],
+    },
+
+    navbar: {
+
+        options: {
+
+        },
+        files: [{
+            expand: true,
+            cwd: './src/app/navbar/i18n',
+            dest: './src/app/navbar/i18n',
+            src: [
+                '**/*.json'
+            ]
+        }],
+    },
+
+    messenger: {
+        
+        options: {
+
+        },
+        files: [{
+            expand: true,
+            cwd: './src/app/main/messenger/i18n',
+            dest: './src/app/main/messenger/i18n',
+            src: [
+                '**/*.json'
+            ]
+        }],
+    },
+
+    chat: {
+        
+        options: {
+
+        },
+        files: [{
+            expand: true,
+            cwd: './src/app/main/messenger/chat/i18n',
+            dest: './src/app/main/messenger/chat/i18n',
+            src: [
+                '**/*.json'
+            ]
+        }],
+    },
+
+    store: {
+        
+        options: {
+
+        },
+        files: [{
+            expand: true,
+            cwd: './src/app/main/store/i18n',
+            dest: './src/app/main/store/i18n',
+            src: [
+                '**/*.json'
+            ]
+        }],
+    }
+}
